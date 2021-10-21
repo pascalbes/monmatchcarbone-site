08795aaa8ef7866cb2556513446746fd74e527b5
Pascal Besson <pascalbes@gmail.com>
2021-10-21 09:47:18 +0200

    UI fix accessibilité

--- source/sites/publicodes/Landing.js
+++ source/sites/publicodes/Landing.js
@@ -41,79 +41,88 @@ export default () => {
 			<div css={`
 				width: 100%;
 				padding: 10px;
-				height: 60%;
+				height: 55%;
 				display: flex;
 				flex-direction: column;
 				align-items:center;
-				justify-content: space-between;
 				background-image: url("fond_stade.png");
+				> div {
+					position: absolute;
+					top: 28%;
+					transform: translate(0, -40%);
+				}
 				h1 {
 					color: white;
 				}
 			`}>
 				<MMCWhite width="200px" />
 				<MMCMonument />
-				<div
-					css={`
-						font-size: 14px;
-						line-height: 20px;
-						width: 80%;
-						display: flex;
-						align-items: center;
-						justify-content: center;
-						color:white;
-						a:visited {
-							color: white;
-						}
-						@media (max-width: 800px) {
-							font-size: 10px;
-							line-height: 14px;
-						}
-					`}
-				>
-					<p>Cette déclinaison, développée par <a href="https://www.gamearth.green/" target="_blank">Game Earth</a> avec <a href="https://www.bl-evolution.com/" target="_blank">BL Évolution</a>, s'appuie librement sur la version officielle de <a href="https://nosgestesclimat.fr/" target="_blank">Nos Gestes Climat</a> développé par <a href="https://www.associationbilancarbone.fr/" target="_blank">l'ABC</a> et <a href="https://datagir.ademe.fr/" target="_blank">Datagir (ADEME)</a>.</p>
-				</div>
 			</div>
 
 			<div css={`
-						width: 100%;
-						height: 20%;
-						background-color: var(--darkColor);
-						a {
-							text-decoration: none;
-							width: 100%;
-							height: 100%;
-							display: flex;
-							flex-direction: column;
-							justify-content: center;
-							align-items: center;
-						}
-						p {
-							color: #62AF9B;
-							margin: 0;
-							font-size: 40px;
-						}
-						:hover {
-							background-color: #62AF9B;
-						}
-						:hover p {
-							color: var(--darkColor);
-						}
-						@media (max-width: 800px) {
-							p {
-								font-size: 20px;
-							}
-						}
-					`}>
+				width: 100%;
+				height: 20%;
+				background-color: var(--darkColor);
+				a {
+					text-decoration: none;
+					width: 100%;
+					height: 100%;
+					display: flex;
+					flex-direction: column;
+					justify-content: center;
+					align-items: center;
+				}
+				p {
+					color: #62AF9B;
+					margin: 0;
+					font-size: 40px;
+				}
+				:hover {
+					background-color: #62AF9B;
+				}
+				:hover p {
+					color: var(--darkColor);
+				}
+				@media (max-width: 800px) {
+					p {
+						font-size: 20px;
+					}
+				}
+			`}>
 				<Link to="/simulateur/bilan">
 					<p>Commencez votre match ></p>
 				</Link>
 			</div>
 			
+			<div
+				css={`
+					margin:0;
+					font-size: 14px;
+					height: 60px;
+					line-height: 20px;
+					width: 100%;
+					padding: 5px 10%;
+					display: flex;
+					align-items: center;
+					justify-content: center;
+					background-color:#62AF9B;
+					color:black;
+					a:visited {
+						color: black;
+					}
+					@media (max-width: 800px) {
+						padding: 2px 5%;
+						font-size: 9px;
+						line-height: 16px;
+					}
+				`}
+			>
+				<p>Cette déclinaison, développée par <a href="https://www.gamearth.green/" target="_blank">Game Earth</a> avec <a href="https://www.bl-evolution.com/" target="_blank">BL Évolution</a>, s'appuie librement sur la version officielle de <a href="https://nosgestesclimat.fr/" target="_blank">Nos Gestes Climat</a> développé par <a href="https://www.associationbilancarbone.fr/" target="_blank">l'ABC</a> et <a href="https://datagir.ademe.fr/" target="_blank">Datagir (ADEME)</a>.</p>
+			</div>
 
 			<footer css={`
 						width: 100%;
-						height: 20%;
+						height: calc(25% - 60px);
 					`}>			
 				<div
 					css={`
